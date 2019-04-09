8e407e291514b5f5958dab28591192174fcd9a62
adamrysyk <rysyk.adam@gmail.com>
2019-04-08 21:55:36 -0400

    add event listeners to app controller

--- app.js
+++ app.js
@@ -9,7 +9,26 @@ const uiController = (() => {
 
 const appController = ((budgetCtrl, uiCtrl) => {
 
-    document.querySelector('')
+    const addItem = () => {
+        // 1. get field input data
 
+        // 2. add item to budget controller
 
+        // 3. add new item to ui
+
+        // 4. calculate budget
+
+        // display budget on ui
+
+        console.log('add an item')
+    }
+
+    document.querySelector('.add-btn ').addEventListener('click', addItem);
+
+    document.addEventListener('keypress', (event) => {
+        // event.which is needed for legacy browsers
+        if (event.keyCode === 13 || event.which === 13) {
+            addItem();
+        }
+    });
 })(budgetController, uiController );
